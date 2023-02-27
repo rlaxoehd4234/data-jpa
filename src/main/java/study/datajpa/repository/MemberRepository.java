968019a41070c32a8696e89a6e48fe78e9c9e032
kimtaedong <rlaxoehd4234@naver.com>
2023-02-27 10:31:51 +0900

    add age query method

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 스프링 데이터 JPA 가 구현체를 만들어서 구현체를 주입 해주는 것이다.
+    public List<Member> findMemberByAge(int age);
 }
