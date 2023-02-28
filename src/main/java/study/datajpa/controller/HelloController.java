2832db2f21d88e25288fd1970496855ed4ea3278
kimtaedong <rlaxoehd4234@naver.com>
2023-02-28 09:58:52 +0900

    master branch test

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     //git branch test
+    //master branch merge test
     @GetMapping("/")
     public String hello(){
         return "hello";
