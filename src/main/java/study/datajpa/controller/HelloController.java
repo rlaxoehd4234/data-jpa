2b71373d192c73d93cdc1321c0ca19c00210dce7
kimtaedong <rlaxoehd4234@naver.com>
2023-02-28 09:53:19 +0900

    git branch test

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    //git branch test
     @GetMapping("/")
     public String hello(){
         return "hello";
