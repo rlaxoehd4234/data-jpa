ca630af1778984c81ed0e8675eb7c55e888a2d32
kimtaedong <rlaxoehd4234@naver.com>
2023-03-08 15:28:10 +0900

    //Repo detail

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // 스프링 데이터 JPA 가 구현체를 만들어서 구현체를 주입 해주는 것이다.
+    // 순수 JPA가 아닌 DataJpa가 기본 CRUD 를 지원해준다.
+    // 굳이 반복되는 CRUD 코드들을 짤 필요없이 다해준다. 필요시에 JPQL 을 추가하자. 
     public List<Member> findMemberByAge(int age);
 
     @Query("select m from Member m where m.name = :username and m.age = :age")
